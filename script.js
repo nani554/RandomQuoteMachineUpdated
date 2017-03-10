f2241ca7ad8ea4e4a3fcf3af73f1525a844bb235
manojkiran <manoj.y554@gmail.com>
2017-03-10 20:16:47 +0530

    updated random quote machine

---
+++ script.js
@@ -0,0 +1,39 @@
+
+$(function(){
+	var quote,author;
+		function getNewQuote(){
+			$.ajax({
+				//making an ajax call to get api
+				jsonp:'jsonp',
+				dataType:'jsonp',
+				url: "http://api.forismatic.com/api/1.0/",
+				data: {
+					method: 'getQuote',
+					lang: 'en',
+					format: 'jsonp'
+				},
+				success: function(response){
+					// success is a callback
+					console.log(response.quoteText);
+					quote = response.quoteText;
+					author = response.quoteAuthor;
+					$("#quote").text(quote);
+					if(author){
+						//if there is any string in author then show this 
+						$("#author").text('said by '+ author);
+					}
+					else{
+						$("#author").text("-Unknown");
+					}
+				}
+			});
+		}
+	$(".get-quote").click(function(event){
+		event.preventDefault();//it prevents "page up" when browser refreshed
+		getNewQuote();
+	})
+	getNewQuote();
+	$(".share-quote").on("click",function(){
+		window.open("https://www.twitter.com/intent/tweet?text="+encodeURIComponent(quote+"--"+author));
+	})
+})
